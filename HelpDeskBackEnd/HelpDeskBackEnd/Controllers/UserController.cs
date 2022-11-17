3a975ec0d1381eebcab27876527e04806c581049
Al Camargo <alcamargo@rocketmortgage.com>
2022-11-17 15:48:37 -0500

    working backend w/ 2 controllers

---
+++ HelpDeskBackEnd/HelpDeskBackEnd/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskBackEnd
+{
+    [Route("user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> GetAll()
+        {
+            return DAL.GetAllUsers();
+        }
+
+        [HttpPost]
+        public User AddUser(User user)
+        {
+            return DAL.AddUser(user);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteUser(int id)
+        {
+            DAL.DeleteUser(id);
+        }
+
+        [HttpPut]
+        public void UpdateUser(User user)
+        {
+            DAL.UpdateUser(user);
+        }
+    }
+}
