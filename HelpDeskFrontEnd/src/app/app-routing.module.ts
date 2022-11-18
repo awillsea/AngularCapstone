0d28730871f3ebf2e9943f8dc6e19b990303d01a
Al Camargo <alcamargo@rocketmortgage.com>
2022-11-18 14:15:50 -0500

    routing and buttons available

--- HelpDeskFrontEnd/src/app/app-routing.module.ts
+++ HelpDeskFrontEnd/src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { FavortiesListComponent } from './favorties-list/favorties-list.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+
+const routes: Routes = [
+
+  {path:'',component:UserListComponent},
+
+  {path:'tickets',component:TicketListComponent},
+
+  {path:'favorites',component:FavortiesListComponent}
+
+];
 
 
-
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
+@NgModule({   imports: [RouterModule.forRoot(routes)],   exports: [RouterModule] })
 export class AppRoutingModule { }
